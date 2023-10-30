cd958919a857335fd798cb6f9bab23353dc63392
user <ap.91@mail.ru>
2023-10-30 17:46:11 +0300

    refactor: change view

--- app/src/main/java/com/example/jcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/jcomposeapplication/MainActivity.kt
@@ -3,23 +3,43 @@ package com.example.jcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val randomNumber = remember { mutableStateOf(0) }
-            Text(randomNumber.value.toString())
-            Button(onClick = { randomNumber.value = Random.nextInt(0, 100) }) {
-                Text(text = "Random number")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            )
+            {
+                val randomNumber = remember { mutableStateOf(0) }
+                Button(modifier = Modifier
+                    .height(99.dp)
+                    .width(300.dp),
+                    onClick = { randomNumber.value = Random.nextInt(0, 100) }
+                )
+                {
+                    Text(text = "Random number", fontSize = 24.sp)
+                }
+                Text(randomNumber.value.toString(), fontSize = 96.sp, color = Color.Gray)
             }
         }
     }
